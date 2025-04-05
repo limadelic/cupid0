c429d81667468c523ee7e48363e045ef6bdb4fcd
msuarz <mikemps@gmail.com>
2025-04-05 15:07:11 -0400

    style: fix spacing

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -1,27 +1,22 @@
 defmodule Scraper.IndeedTest do
   use ExUnit.Case
-
   @job_key "e3d5735de501774d"
   @base_url "https://www.indeed.com/viewjob"
 
-  describe "view_job url" do
-    test "starts with indeed.com/viewjob" do
-      url = view_url()
-      assert String.starts_with? url, @base_url
-    end
+  test "builds url" do
+    url = view_url cmp: "One-Inc", t: "Software Engineer"
 
-    test "includes params" do
-      url = view_url cmp: "One-Inc", t: "Software Engineer"
-      assert has_param? url, :cmp, "One-Inc"
-      assert has_param? url, :t, "Software Engineer"
-    end
+    assert String.starts_with? url, @base_url
+    assert has_param? url, :jk, @job_key
+    assert has_param? url, :cmp, "One-Inc"
+    assert has_param? url, :t, "Software Engineer"
   end
 
-  defp view_url(params \\ []) do
-    Scraper.Indeed.view_job([jk: @job_key] ++ params)
+  defp view_url params \\ [] do
+    Scraper.Indeed.view_job [jk: @job_key] ++ params
   end
 
-  defp has_param?(url, key, value) do
+  defp has_param? url, key, value do
     String.contains? url, "#{key}=#{URI.encode_www_form value}"
   end
 end
