e7385ee6e2694ec9a9a079405f8e40d73df03322
msuarz <mikemps@gmail.com>
2025-04-05 14:19:24 -0400

    green: add base url

--- cupid0/apps/scraper/lib/scraper/indeed.ex
+++ cupid0/apps/scraper/lib/scraper/indeed.ex
@@ -1,8 +1,10 @@
 defmodule Scraper.Indeed do
+  @base_url "https://www.indeed.com"
+
   @doc """
   Builds a search URL for Indeed with the given parameters.
   Params:
     - q: search term
   """
-  def search_url(_params \\ []), do: ""
+  def search_url(_params \\ []), do: @base_url
 end
--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -1,7 +1,7 @@
 defmodule Scraper.IndeedTest do
   use ExUnit.Case
 
-  test "search_url accepts a search term" do
-    assert is_binary(Scraper.Indeed.search_url(q: "elixir"))
+  test "search_url starts with indeed.com" do
+    assert String.starts_with?(Scraper.Indeed.search_url(q: "elixir"), "https://www.indeed.com")
   end
 end
