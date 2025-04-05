cc8804a1c724b09a396c3b4bf53338a1a5093067
msuarz <mikemps@gmail.com>
2025-04-05 14:59:50 -0400

    refactor: clean up test repetition

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -1,23 +1,27 @@
 defmodule Scraper.IndeedTest do
   use ExUnit.Case
 
-  test "view_job starts with indeed.com/viewjob" do
-    url = Scraper.Indeed.view_job jk: "e3d5735de501774d"
-    assert String.starts_with? url, "https://www.indeed.com/viewjob"
+  @job_key "e3d5735de501774d"
+  @base_url "https://www.indeed.com/viewjob"
+
+  describe "view_job url" do
+    test "starts with indeed.com/viewjob" do
+      url = view_url()
+      assert String.starts_with? url, @base_url
+    end
+
+    test "includes params" do
+      url = view_url cmp: "One-Inc", t: "Software Engineer"
+      assert has_param? url, :cmp, "One-Inc"
+      assert has_param? url, :t, "Software Engineer"
+    end
   end
 
-  test "view_job includes job key" do
-    url = Scraper.Indeed.view_job jk: "e3d5735de501774d"
-    assert String.contains? url, "jk=e3d5735de501774d"
+  defp view_url(params \\ []) do
+    Scraper.Indeed.view_job([jk: @job_key] ++ params)
   end
 
-  test "view_job includes company" do
-    url = Scraper.Indeed.view_job jk: "e3d5735de501774d", cmp: "One-Inc"
-    assert String.contains? url, "cmp=One-Inc"
-  end
-
-  test "view_job includes title" do
-    url = Scraper.Indeed.view_job jk: "e3d5735de501774d", t: "Software Engineer"
-    assert String.contains? url, "t=Software+Engineer"
+  defp has_param?(url, key, value) do
+    String.contains? url, "#{key}=#{URI.encode_www_form value}"
   end
 end
