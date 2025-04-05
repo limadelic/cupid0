623cc99b0b16a256c092947090f1128c03e95093
msuarz <mikemps@gmail.com>
2025-04-05 15:19:26 -0400

    test: make it concise

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -1,29 +1,9 @@
 defmodule Scraper.IndeedTest do
-
   use ExUnit.Case
 
   test "view job url" do
-    # Given a job with key, company and title
-    job = [
-      jk: "e3d5735de501774d",
-      cmp: "One-Inc",
-      t: "Software Engineer"
-    ]
-
-    # When viewing the job
-    url = Scraper.Indeed.view_job job
-
-    # Then url should start with indeed viewjob
+    url = Scraper.Indeed.view_job jk: "123", cmp: "inc", t: "dev"
     assert String.starts_with? url, "https://www.indeed.com/viewjob"
-
-    # And include all job params
-    assert has? url, :jk, "e3d5735de501774d"
-    assert has? url, :cmp, "One-Inc"
-    assert has? url, :t, "Software Engineer"
+    assert String.contains? url, "cmp=inc&jk=123&t=dev"
   end
-
-  defp has? url, key, value do
-    String.contains? url, "#{key}=#{URI.encode_www_form value}"
-  end
-
 end
