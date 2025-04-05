0dc0211d4fd8430d2b03fe441ed320f44cf56981
msuarz <mikemps@gmail.com>
2025-04-05 16:07:39 -0400

    test: parse job titles

--- cupid0/apps/scraper/lib/scraper/indeed.ex
+++ cupid0/apps/scraper/lib/scraper/indeed.ex
@@ -3,8 +3,11 @@ defmodule Scraper.Indeed do
   @http Application.compile_env(:scraper, :http_client, HTTPoison)
 
   def search term do
-    @http.get "https://www.indeed.com/jobs?q=#{term}"
-    []
+    {:ok, %{body: html}} = @http.get "https://www.indeed.com/jobs?q=#{term}"
+    html
+    |> String.split("<job>")
+    |> Enum.drop(1)
+    |> Enum.map(&String.replace(&1, "</job>", ""))
   end
 
 end
