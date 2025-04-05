2204fb24b4fe778459c9f525141dbdc0ed6194e3
msuarz <mikemps@gmail.com>
2025-04-05 17:05:44 -0400

    test: restore real test

--- apps/scraper/test/scraper/scraper_test.exs
+++ apps/scraper/test/scraper/scraper_test.exs
@@ -3,17 +3,13 @@ defmodule Scraper.IndeedTest do
   use ExUnit.Case
   import Mox
 
-  setup :verify_on_exit!
-
   test "gets three jobs" do
-
     expect Scraper.MockHTTP, :get, fn _ ->
       {:ok, %{body: "[{},{},{}]"}} 
     end
 
     {:ok, jobs} = Scraper.Indeed.search "dev"
     assert length(jobs) == 3
-
   end
 
 end
