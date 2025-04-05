8cd9469e1c3396468270e589cf21ced246d0388d
msuarz <mikemps@gmail.com>
2025-04-05 16:44:53 -0400

    test: verify any response

--- apps/scraper/test/scraper/scraper_test.exs
+++ apps/scraper/test/scraper/scraper_test.exs
@@ -3,13 +3,14 @@ defmodule Scraper.IndeedTest do
   use ExUnit.Case
   import Mox
 
-  test "calls indeed" do
+  test "gets response" do
 
     expect Scraper.HTTP, :get, fn _ ->
       {:ok, %{body: "ok"}} 
     end
 
-    Scraper.Indeed.search "dev"
+    response = Scraper.Indeed.search "dev"
+    assert is_binary(response)
 
   end
 
