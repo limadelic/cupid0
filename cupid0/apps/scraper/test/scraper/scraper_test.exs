3e7b3fb50a6bf884648778fec93653c42f32be63
msuarz <mikemps@gmail.com>
2025-04-05 15:55:21 -0400

    style: improve test readability

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -3,13 +3,15 @@ defmodule Scraper.IndeedTest do
   use ExUnit.Case
   import Mox
 
-  test "search calls indeed" do
+  test "finds developer jobs" do
+
     expect Scraper.HTTP, :get, fn url ->
       assert url == "https://www.indeed.com/jobs?q=dev"
       {:ok, %{body: ""}}
     end
 
     Scraper.Indeed.search "dev"
+
   end
 
 end
