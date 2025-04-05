da5e8378018411305d977d06d5325b0ba3c55989
msuarz <mikemps@gmail.com>
2025-04-05 15:20:34 -0400

    style: fix spacing

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -1,9 +1,13 @@
 defmodule Scraper.IndeedTest do
+
   use ExUnit.Case
 
   test "view job url" do
+
     url = Scraper.Indeed.view_job jk: "123", cmp: "inc", t: "dev"
     assert String.starts_with? url, "https://www.indeed.com/viewjob"
     assert String.contains? url, "cmp=inc&jk=123&t=dev"
+
   end
+
 end
