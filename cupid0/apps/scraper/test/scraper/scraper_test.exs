96be7a75e966abe606e952c516c6feb2eb7b44cf
msuarz <mikemps@gmail.com>
2025-04-05 15:22:50 -0400

    test: improve behavior description

--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -2,7 +2,7 @@ defmodule Scraper.IndeedTest do
 
   use ExUnit.Case
 
-  test "links to job details" do
+  test "finds jobs on indeed" do
 
     url = Scraper.Indeed.view_job jk: "123", cmp: "inc", t: "dev"
     assert String.starts_with? url, "https://www.indeed.com/viewjob"
