0f9d09b259798161a7017201a86a7857af78d6a4
msuarz <mikemps@gmail.com>
2025-04-05 18:28:20 -0400

    refactor: fix test structure

--- apps/scraper/test/specs/indeed_spec.exs
+++ apps/scraper/test/specs/indeed_spec.exs
@@ -1,8 +1,20 @@
 defmodule Scraper.IndeedSpec do
   use ExUnit.Case
+  import Mox
 
-  test "parses job title from html" do
-    html = "<title>Software Engineer - One Inc</title>"
-    assert Scraper.Indeed.parse_title(html) == "Software Engineer"
+  setup :verify_on_exit!
+
+  setup do
+    Application.put_env :scraper, :http_client, Scraper.MockHTTP
+    :ok
+  end
+
+  test "gets three jobs" do
+    expect Scraper.MockHTTP, :get, fn _url ->
+      {:ok, %{body: "[{},{},{}]"}} 
+    end
+
+    {:ok, jobs} = Scraper.Indeed.search "dev"
+    assert length(jobs) == 3
   end
 end
