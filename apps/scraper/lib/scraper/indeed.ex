3021fdd29edd9d5d26b05938d8d7e10e58948898
msuarz <mikemps@gmail.com>
2025-04-05 17:48:35 -0400

    refactor: use runtime config

--- apps/scraper/lib/scraper/indeed.ex
+++ apps/scraper/lib/scraper/indeed.ex
@@ -1,18 +1,23 @@
 defmodule Scraper.Indeed do
+
   @behaviour Scraper.HTTP
 
   def get url do
 
-    http_client = Application.get_env :scraper, :http_client
-    http_client.get url
+    http_client().get url
 
   end
 
   def search term do
 
-    http_client = Application.get_env :scraper, :http_client
-    {:ok, %{body: json}} = http_client.get "https://www.indeed.com/jobs?q=#{term}"
+    {:ok, %{body: json}} = http_client().get "https://www.indeed.com/jobs?q=#{term}"
     {:ok, Jason.decode! json}
+
+  end
+
+  defp http_client do
+
+    Application.get_env :scraper, :http_client
 
   end
 
--- rules/impl.md
+++ rules/impl.md
@@ -1,5 +1,3 @@
 1. Keep behavior interfaces minimal
 2. Keep implementations close to data
 3. Minimize repetition
-4. Avoid speculative features
-5. Code is for humans first
