bfabaad1846d03a9cbc1ebf8a89c09667b1b5b10
msuarz <mikemps@gmail.com>
2025-04-05 15:02:33 -0400

    clean: simplify code

--- cupid0/apps/scraper/lib/scraper/indeed.ex
+++ cupid0/apps/scraper/lib/scraper/indeed.ex
@@ -2,11 +2,24 @@ defmodule Scraper.Indeed do
   @base_url "https://www.indeed.com"
 
   def view_job params \\ [] do
-    query = %{
-      jk: Keyword.get(params, :jk, ""),
-      cmp: Keyword.get(params, :cmp, ""),
-      t: Keyword.get(params, :t, "")
+    params
+    |> query
+    |> url
+  end
+
+  defp query params do
+    %{
+      jk: param(params, :jk),
+      cmp: param(params, :cmp),
+      t: param(params, :t)
     }
-    @base_url <> "/viewjob?" <> URI.encode_query(query)
+  end
+
+  defp param params, key, default \\ "" do
+    Keyword.get params, key, default
+  end
+
+  defp url params do
+    @base_url <> "/viewjob?" <> URI.encode_query params
   end
 end
