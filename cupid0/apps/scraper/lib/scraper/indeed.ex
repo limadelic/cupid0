d7093940a155dc408e0ce897c43c2e129f6e9539
msuarz <mikemps@gmail.com>
2025-04-05 15:52:14 -0400

    red: simplest search function

--- cupid0/apps/scraper/lib/scraper/indeed.ex
+++ cupid0/apps/scraper/lib/scraper/indeed.ex
@@ -1,33 +1,7 @@
 defmodule Scraper.Indeed do
 
-  @base_url "https://www.indeed.com"
-  @path "/jobs"
-
-  def search params do
-    params
-    |> query
-    |> url
-  end
-
-  defp query params do
-    %{
-      q: param(params, :q)
-    }
-  end
-
-  defp param params, key do
-    Keyword.get params, key, ""
-  end
-
-  defp url params do
-    [@base_url, @path, "?", encode(params)]
-    |> Enum.join
-  end
-
-  defp encode params do
-    params
-    |> Enum.map(fn {k, v} -> "#{k}=#{URI.encode_www_form v}" end)
-    |> Enum.join("&")
+  def search _term do
+    []
   end
 
 end
--- cupid0/apps/scraper/test/scraper/scraper_test.exs
+++ cupid0/apps/scraper/test/scraper/scraper_test.exs
@@ -2,12 +2,8 @@ defmodule Scraper.IndeedTest do
 
   use ExUnit.Case
 
-  test "search for jobs" do
-
-    url = Scraper.Indeed.search q: "dev"
-    assert String.starts_with? url, "https://www.indeed.com/jobs"
-    assert String.contains? url, "q=dev"
-
+  test "search returns list" do
+    assert is_list Scraper.Indeed.search "dev"
   end
 
 end
