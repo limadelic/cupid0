415da7311447ec012cba834f9301965983529f9e
msuarz <mikemps@gmail.com>
2025-04-05 20:11:37 -0400

    clean: start fresh with pending steps

--- apps/scraper/test/features/indeed_jobs_test.exs
+++ apps/scraper/test/features/indeed_jobs_test.exs
@@ -1,30 +1,23 @@
-defmodule Scraper.Examples.IndeedJobsTest do
+defmodule Scraper.Features.IndeedJobsTest do
   use Cabbage.Feature, async: false, file: "indeed_jobs.feature"
 
   setup do
-    {:ok, %{}} # Return initial state
+    {:ok, %{}}
   end
 
   defgiven ~r/^I want to search for remote software engineering jobs$/, _, state do
-    {:ok, Map.put(state, :base_url, "https://www.indeed.com/jobs")}
+    {:ok, %{url: "https://www.indeed.com/jobs"}}
   end
 
-  defwhen ~r/^I search with these criteria:$/, %{table: criteria}, state do
-    position = Enum.find_value(criteria, fn row ->
-      row["Position"]
-    end)
-    work_type = Enum.find_value(criteria, fn row ->
-      row["Work Type"]
-    end)
-    query = %{q: position, rbl: work_type}
-    {:ok, Map.put(state, :query, query)}
+  defwhen ~r/^I search with these criteria:$/, %{table: _criteria}, state do
+    {:ok, state}
   end
 
   defthen ~r/^I should get a list of matching job postings$/, _, state do
     {:ok, state}
   end
 
-  defthen ~r/^each job posting should have:$/, %{table: fields}, state do
+  defthen ~r/^each job posting should have:$/, %{table: _fields}, state do
     {:ok, state}
   end
 end
