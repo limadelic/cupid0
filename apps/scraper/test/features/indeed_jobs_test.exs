4d72f79180dcac70818562ff5d64cb3df0291dc7
msuarz <mikemps@gmail.com>
2025-04-06 09:27:45 -0400

    test: use html parsing

--- apps/scraper/test/features/indeed_jobs_test.exs
+++ apps/scraper/test/features/indeed_jobs_test.exs
@@ -6,24 +6,19 @@ defmodule Scraper.Features.IndeedJobsTest do
     {:ok, %{}}
   end
 
-  defgiven ~r/^I want to search for remote software engineering jobs$/, _, state do
-    {:ok, %{url: "https://www.indeed.com/jobs"}}
+  defgiven ~r/^I want to search for remote software engineering jobs$/, _, _ do
+    :ok
   end
 
-  defwhen ~r/^I search with these criteria:$/, %{table: criteria}, state do
-    
-    query = %{
-      q: wants(criteria, "Position"),
-      rbl: wants(criteria, "Work Type"),
-      salaryType: wants(criteria, "Salary")
-    }
-
-    {:ok, Map.put(state, :query, query)}
-
+  defwhen ~r/^I search with these criteria:$/, %{table: criteria}, _ do
+    term = wants(criteria, "Position")
+    {:ok, %{body: html}} = Scraper.Indeed.get("https://www.indeed.com/jobs?q=#{term}")
+    _title = Scraper.Indeed.parse_title(html)
+    :ok
   end
 
-  defthen ~r/^I should get a list of matching job postings$/, _, state do
-    {:ok, state}
+  defthen ~r/^I should get a list of matching job postings$/, _, _ do
+    :ok
   end
 
   defthen ~r/^each job posting should have:$/, %{table: _fields}, state do
