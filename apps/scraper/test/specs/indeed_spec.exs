044aba617895ccaf85f342b81c7b72b74150b2b4
msuarz <mikemps@gmail.com>
2025-04-06 10:48:42 -0400

    test: prepare for mocking

--- apps/scraper/test/specs/indeed_spec.exs
+++ apps/scraper/test/specs/indeed_spec.exs
@@ -8,25 +8,23 @@ defmodule Scraper.IndeedSpec do
   setup do
 
     Application.put_env :scraper, :http_client, Scraper.MockHTTP
-    expect Scraper.MockHTTP, :get, fn url ->
-      send self(), {:get, url}
-      {:ok, %{body: ""}}
-    end
+    # expect Scraper.MockHTTP, :get, fn url ->
+    #   send self(), {:get, url}
+    #   {:ok, %{body: ""}}
+    # end
     :ok
 
   end
 
   test "search with criteria" do
 
-    {:ok, jobs} = Scraper.Indeed.search(
+    {:ok, _} = Scraper.Indeed.search(
       position: "software engineer",
       work_type: "Remote",
       salary: "$135,000+",
       posted: "Last 24 hours",
       sort: "Date"
     )
-
-    assert length(jobs) > 0
 
   end
 
--- apps/scraper/test/test_helper.exs
+++ apps/scraper/test/test_helper.exs
@@ -1,4 +1,6 @@
 ExUnit.start()
 {:ok, _} = Application.ensure_all_started(:cabbage)
 
+Code.require_file Path.join(__DIR__, "specs/indeed_spec.exs")
+
 Mox.defmock Scraper.MockHTTP, for: Scraper.HTTP
