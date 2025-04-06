1b93f5f48c8e3544743b7a9c935e1b1ef7c0145f
msuarz <mikemps@gmail.com>
2025-04-05 20:15:18 -0400

    refactor: extract criteria helper

--- apps/scraper/test/features/indeed_jobs_test.exs
+++ apps/scraper/test/features/indeed_jobs_test.exs
@@ -10,16 +10,12 @@ defmodule Scraper.Features.IndeedJobsTest do
   end
 
   defwhen ~r/^I search with these criteria:$/, %{table: criteria}, state do
-    position = Enum.find_value(criteria, fn row ->
-      row["Position"]
-    end)
-    work_type = Enum.find_value(criteria, fn row ->
-      row["Work Type"]
-    end)
-    salary = Enum.find_value(criteria, fn row ->
-      row["Salary"]
-    end)
-    {:ok, Map.put(state, :query, %{q: position, rbl: work_type, salaryType: salary})}
+    query = %{
+      q: get_criteria(criteria, "Position"),
+      rbl: get_criteria(criteria, "Work Type"),
+      salaryType: get_criteria(criteria, "Salary")
+    }
+    {:ok, Map.put(state, :query, query)}
   end
 
   defthen ~r/^I should get a list of matching job postings$/, _, state do
@@ -28,5 +24,9 @@ defmodule Scraper.Features.IndeedJobsTest do
 
   defthen ~r/^each job posting should have:$/, %{table: _fields}, state do
     {:ok, state}
+  end
+
+  defp get_criteria(criteria, field) do
+    Enum.find_value(criteria, fn row -> row[field] end)
   end
 end
