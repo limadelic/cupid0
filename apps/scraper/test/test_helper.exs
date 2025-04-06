220be6bcc2e044fe09356389b2a1b1c792f076c9
msuarz <mikemps@gmail.com>
2025-04-06 13:39:30 -0400

    fix: remove duplicate HTTP module definition

--- apps/scraper/test/test_helper.exs
+++ apps/scraper/test/test_helper.exs
@@ -3,9 +3,4 @@ ExUnit.start()
 
 Code.require_file Path.join(__DIR__, "specs/indeed_spec.exs")
 
-defmodule Scraper.HTTP do
-  @callback get(String.t()) :: {:ok, map()} | {:error, term()}
-  @callback get(String.t(), list()) :: {:ok, map()} | {:error, term()}
-end
-
 Mox.defmock Scraper.MockHTTP, for: Scraper.HTTP
